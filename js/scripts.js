f1dcc54f739d9c6eaed881b5d33120e1f64ec6a6
nehakhan77 <nehakhann71@gmail.com>
2023-08-21 21:57:55 -0400

    Changed from for block to foreach function

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,7 @@ let repository = [
     { name: "Squirtle", type: "water", height: "12" }
 ];
 
-//A loop to display all list items using the "forEach Loop" format, including a conditional to check if height is within a certain value between 15 and 10.
-for (let i = 0; i < repository.length; i++) {
-    document.write(repository[i].name + " is " + repository[i].height + ' ');
-    if (repository[i].height > 10) {
-        document.write("-Wow that's big!")
-    };
-}
+repository.foreach(function (pokemonlist) {
+    console.log(pokemonlist);
+});
+
