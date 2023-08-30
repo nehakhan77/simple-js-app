9bac83eefe265831bdc04240557ad85584e92543
nehakhan77 <nehakhann71@gmail.com>
2023-08-30 15:50:42 -0400

    Changed from for block to for each function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 let repository = [
-    { name: "Bulbasaur", type: "grass", height: "7" },
-    { name: "Charmander", type: "fire", height: "2" },
+    { name: "Bulbasaur", type: "grass", height: "7" }
+    { name: "Charmander", type: "fire", height: "2" }
     { name: "Squirtle", type: "water", height: "12" }
 ];
 
-repository.foreach(function (pokemonlist) {
-    console.log(pokemonlist);
-});
+document.write(myName);
+let myName = 'Neha';
 
